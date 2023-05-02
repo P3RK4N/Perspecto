694f4eaf546fc5721ecbc7814f4744c6b3434b6f
Bruno Perković <perkanperkovic15@gmail.com>
2023-05-02 16:51:33 +0200

    Unity project init

--- src/helper.ts
+++
@@ -1,6 +0,0 @@
-export const CheckWebGPU = () =>
-{
-   let result = "Your browser supports WebGPU!";
-   if(!navigator.gpu) result = "Your browser does not support WebGPU!";
-   return result;
-}
--- src/types/shader.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.wgsl'; 
-declare module '*.gsl';  
-declare module '*.vs';   
-declare module '*.fs';   
